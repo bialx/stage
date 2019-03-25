e7d12be3a049d60515758d766ccaa474535c6a35
bialx <coulaud.jeremie@laposte.net>
2019-03-25 17:17:21 +0100

    add another test for squaring

--- first_implem/main_avx2.c
+++ first_implem/main_avx2.c
@@ -13,7 +13,7 @@ int main() {
 
   uint64_t tab1[4] = {1534345, 14300953, 104002000, 193294};
   uint64_t tab2[4] = {4843838, 74646, 89878971, 77777777777};
-  uint64_t result[2] = {1152939096822257163, 65536};
+  uint64_t result[2] = {1188967911021090315, 16777248};
   uint64_t result2[1] = {1073873161};
   __m128i output1[2] = {_mm_setzero_si128(), _mm_setzero_si128()};
   uint64_t output2[8] = {0};
@@ -32,8 +32,8 @@ int main() {
       printf("output1[%d][%d]: %lld\n", i, j, output1[i][j]);
     }
   }
-  /* output expected: 369435925020741 72057594054705152 4294967296 0 */
-
+  /* output expected: 369435925020741 72057594054705152 4294967296  */
+  /* output expected: 369435925020741 72127962798882832 281474977760256 */
   long long int S2 = 0;
   for(int i = 0; i < NBR_TEST; ++i){
     long long t9 = cpucycles();
